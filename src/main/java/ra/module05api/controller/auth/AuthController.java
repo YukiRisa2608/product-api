a8f15a5c059e927f8f870220f9adb1ac3a43d245
YukiRisa2608 <nham26081992@gmail.com>
2024-03-12 22:37:45 +0900

    add

--- src/main/java/ra/module05api/controller/auth/AuthController.java
+++ src/main/java/ra/module05api/controller/auth/AuthController.java
@@ -3,10 +3,7 @@ package ra.module05api.controller.auth;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ra.module05api.dto.request.SignInRequest;
 import ra.module05api.dto.request.SignUpRequest;
 import ra.module05api.dto.response.ResponseDtoSuccess;
@@ -14,7 +11,7 @@ import ra.module05api.dto.response.SignInDtoResponse;
 import ra.module05api.exception.DataFieldExistException;
 import ra.module05api.exception.UsernameOrPasswordException;
 import ra.module05api.service.AuthenticationService;
-
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api.com/v2/auth")  // công khai
 @RequiredArgsConstructor
